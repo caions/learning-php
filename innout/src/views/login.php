805b6b84febba5918ac1b0a295fccc003ec0de83
Caio Nascimento <caionsantos2000@gmail.com>
2022-03-03 15:19:46 -0300

    feat: validation login

--- innout/src/views/login.php
+++ innout/src/views/login.php
@@ -25,6 +25,7 @@
         <i class="icofont-runner-alt-1 ml-2"></i>
       </div>
       <div class="card-body">
+        <?php include(TEMPLATE_PATH . '/messages.php') ?>
         <div class="form-group">
           <label for="email">Email</label>
           <input type="email" id="email" 
