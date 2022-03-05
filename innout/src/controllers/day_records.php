83596a1f79138f525f144b113b8995f3b39d7226
Caio Nascimento <caionsantos2000@gmail.com>
2022-03-05 16:01:18 -0300

    feat: show worked hours

--- innout/src/controllers/day_records.php
+++ innout/src/controllers/day_records.php
@@ -7,10 +7,5 @@ loadModel('WorkingHours');
 $date = (new DateTime())->getTimestamp();
 $today = strftime('%d de %B de %Y', $date);
 
-$user = $_SESSION['user'];
-$records = WorkingHours::loadFromUserAndDate($user->id,date('Y-m-d'));
+loadTemplateView('day_records', ['today' => $today]);
 
-loadTemplateView('day_records',[
-  'today'=> $today,
-  'records' => $records
-]);
