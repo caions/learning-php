cffc7cfbcb1d4f9b24151b9a8d756acc686338e9
Caio Nascimento <caionsantos2000@gmail.com>
2022-03-08 14:56:25 -0300

    feat: user create with validation

--- innout/src/controllers/save_user.php
+++ innout/src/controllers/save_user.php
@@ -1,15 +1,33 @@
 <?php
 session_start();
-requireValidSession();
+requireValidSession(true);
 
-/* $users = User::get();
+$exception = null;
+$userData = [];
 
-foreach ($users as $user) {
-  $user->start_date = (new DateTime($user->start_date))->format('d/m/Y');
-  if ($user->end_date) {
-    $user->end_date = (new DateTime($user->end_date))->format('d/m/Y');
+if (count($_POST) === 0 && isset($_GET['update'])) {
+  $user = User::getOne(['id' => $_GET['update']]);
+  $userData = $user->getValues();
+  $userData['password'] = null;
+} elseif (count($_POST) > 0) {
+  try {
+    $dbUser = new User($_POST);
+    if ($dbUser->id) {
+      $dbUser->update();
+      addSuccessMsg('Usuário alterado com sucesso!');
+      header('Location: users.php');
+      exit();
+    } else {
+      $dbUser->id = null;
+      $dbUser->insert();
+      addSuccessMsg('Usuário cadastrado com sucesso!');
+    }
+    $_POST = [];
+  } catch (Exception $e) {
+    $exception = $e;
+  } finally {
+    $userData = $_POST;
   }
 }
- */
 
-loadTemplateView('save_user');
+loadTemplateView('save_user', $userData + ['exception' => $exception]);
